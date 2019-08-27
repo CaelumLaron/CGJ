69e6b2819bbc556b5e8252063d091501eca8cd29
CaelumLaron <lucas.rocha@icomp.ufam.edu.br>
2019-08-27 13:53:03 -0400

    Player Script

---
+++ Assets/Scripts/player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+	//Settings
+	[Header ("Movement Settings")]
+	[SerializeField] float speed = .2f;
+    
+	//Global Variables
+	Rigidbody2D rg2d;
+
+    void Start(){        
+    	rg2d = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    void Update(){
+    	float xPos = Input.GetAxisRaw("Horizontal");
+    }
+}
